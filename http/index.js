0aebe3de8bd1f76da8a5a8bed58eae1d9b35651e
ariefardi <forzaariefpyon@gmail.com>
2019-08-08 21:23:48 +0700

    change

--- http/index.js
+++ http/index.js
@@ -9,12 +9,12 @@ const index = module.exports.index = (req, res) => {
   const method = req.method;
   const service = url.split('/')[1];
   const request = require(`./${method}`)[method];
-  // console.log('request ', method)
-  // console.log('service ', service)
-  // console.log('url ', service)
-  // request(req, res, url, PORT[service]);
-  res.json({
-    message: "jalan kok"
-  })
+  console.log('request ', method)
+  console.log('service ', service)
+  console.log('url ', service)
+  request(req, res, url, PORT[service]);
+  // res.json({
+  //   message: "jalan kok"
+  // })
 
 };
